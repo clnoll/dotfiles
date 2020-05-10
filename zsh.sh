0f96121406c1c168a641876664a99d88581223d1
Catherine Noll <catherine@happybits.co>
2020-05-10 15:11:49 -0700

    set tmux prefix

--- zsh.sh
+++ zsh.sh
@@ -10,3 +10,5 @@ source ~/dotfiles/zsh_aliases.sh
 export SHELL=/usr/local/bin/zsh
 export HISTFILE=~/.eternal_shell_history
 export PATH=~/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/opt/X11/bin:~/joya/mc-web/N/google-cloud-sdk/bin:~/.cargo/bin:/Applications/SnowSQL.app/Contents/MacOS:$PATH
+
+[ -n "$TMUX" ] && tmux set-option prefix C-z >/dev/null
