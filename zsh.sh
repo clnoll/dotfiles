aca3b0cce903d598a9c6239906fe3eb116c72ade
Catherine Noll <catherine@happybits.co>
2020-05-10 14:56:51 -0700

    Set shell PATH

--- zsh.sh
+++ zsh.sh
@@ -9,4 +9,4 @@ source ~/dotfiles/zsh_aliases.sh
 
 export SHELL=/usr/local/bin/zsh
 export HISTFILE=~/.eternal_shell_history
-export PATH=~/bin:$PATH
+export PATH=~/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/opt/X11/bin:~/joya/mc-web/N/google-cloud-sdk/bin:~/.cargo/bin:/Applications/SnowSQL.app/Contents/MacOS:$PATH
