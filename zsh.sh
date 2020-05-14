b8722d39361ce607829698887c14aa7cd3f6043c
Catherine Noll <catherine@happybits.co>
2020-05-14 12:39:56 -0700

    zsh unset share_history

--- zsh.sh
+++ zsh.sh
@@ -12,3 +12,6 @@ export HISTFILE=~/.eternal_shell_history
 export PATH=~/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/opt/X11/bin:~/joya/mc-web/N/google-cloud-sdk/bin:~/.cargo/bin:/Applications/SnowSQL.app/Contents/MacOS:$PATH
 
 [ -n "$TMUX" ] && tmux set-option prefix C-z >/dev/null
+
+unsetopt share_history
+
