66340a9f06c2ca5ac81b16af484d115999b871cc
Catherine Noll <catherine@happybits.co>
2021-12-20 18:58:58 -0500

    Revert "Create worldclock_utc12hr.1m.sh"
    This reverts commit e3f96c3da98191bb5d70816df1d4840fb5cf24ef.

--- b6ba312/worldclock_utc12hr.1m.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/Library/Application\ Support/xbar/plugins
-cat > ~/Library/Application\ Support/xbar/plugins/worldclock_utc.1m.sh <<EOF
-#!/bin/bash
-zone="America/Los_Angeles"
-echo "\$zone \$(TZ=\$zone date +'%a %I:%M')"
-EOF
-chmod +x ~/Library/Application\ Support/xbar/plugins/worldclock_utc.1m.sh
