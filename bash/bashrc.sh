908eab5ef2c5f83d0d2752470b2f42bb0c83db1a
Catherine Noll <noll.catherine@gmail.com>
2022-11-26 13:43:52 -0500

    Add bash settings, completion, history

---
+++ bash/bashrc.sh
@@ -0,0 +1,41 @@
+_cwd=$(pwd)
+
+export GIT_PS1_UNSTAGED="🐉  "
+export GIT_PS1_STAGED="🎲  "
+
+cd ~/configfiles/
+source lib.sh
+source path.sh
+source git-functions.sh
+source env.sh
+# __dan_is_osx && source osx_env.sh
+source dircolors.sh
+source prompt.sh
+source bash/history.sh
+source bash/completion.sh
+# source fasd.sh
+source readline.sh
+source alias.sh
+[ -f extra.sh ] && source extra.sh
+# source bash/alias_completion.sh
+source tmux.sh
+
+export PROMPT_COMMAND=__prompt_command
+
+cd "$_cwd"
+
+. ~/configfiles/git-functions.sh
+
+export WORKON_HOME=$HOME/.virtualenvs
+alias subl="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
+
+export DOCKER_MACHINE_IP=192.168.99.100
+okh() { sed -i "" "${1}d" ~/.ssh/known_hosts; }
+export EDITOR='emacsclient -n'
+alias e='emacsclient -n'
+alias eg='emacs-magit-status'
+alias emacs-magit='emacs-magit-status'
+
+eval "$(pyenv init -)"
+# . ~/facet/completion/bash/facet
+export JOYA_EMAIL=catherine@happybits.co
