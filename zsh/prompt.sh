a90e81968dc3b4a68d5bd29d5680f8f687d294e1
Catherine Noll <noll.catherine@gmail.com>
2022-11-27 22:00:16 -0500

    clean up

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,6 +1,6 @@
 # Based on oh-my-zsh/themes/robbyrussell.zsh-theme
-# But using __git_ps1 from https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
-source ~/src/3p/git/contrib/completion/git-prompt.sh
+# But using __git_ps1 from https://github.com/dandavison/shell-config/blob/master/3p/git-prompt.sh
+source ~/src/3p/dan-shell-config/3p/git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=yes
 export GIT_PS1_UNSTAGED="🐉  "
 export GIT_PS1_STAGED="🐠  "
--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,10 +1,9 @@
 ### oh-my-zsh config
 export ZSH="/Users/catherine/.oh-my-zsh"
-export SHELL_ARCH=$(arch)
 plugins=(git)
 source $ZSH/oh-my-zsh.sh
 
-### Personal config
+
 source ~/dotfiles/zsh/prompt.sh
 source ~/dotfiles/zsh/aliases.sh
 source ~/dotfiles/lib.sh
@@ -12,18 +11,10 @@ source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 
 export HISTFILE=~/.eternal_shell_history
-export PATH=/opt/X11/bin:~/joya/mc-web/N/google-cloud-sdk/bin:~/.cargo/bin:/Applications/SnowSQL.app/Contents/MacOS:/opt/homebrew/bin/cmake:$PATH
-
-if [ $SHELL_ARCH = "i386" ]
-then
-    export SHELL=/usr/local/bin/zsh
-    export PATH=~/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:$PATH
-    # export TMUX_EXECUTABLE=~/src/3p/tmux/tmux_x86
-else
-    export SHELL=/opt/homebrew/bin/zsh
-    export PATH=~/bin:/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/bin:$PATH
-    # export TMUX_EXECUTABLE=~/src/3p/tmux/tmux_arm
-fi
+export PATH=/opt/X11/bin:~/.cargo/bin:/Applications/SnowSQL.app/Contents/MacOSå:$PATH
+export PATH=~/bin:/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/bin:$PATH
+export SHELL=/opt/homebrew/bin/zsh
+export TMUX_EXECUTABLE=/opt/homebrew/bin/tmux
 
 export EDITOR="emacs -nw -q"
 
